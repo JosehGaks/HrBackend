27d161e1ea5d8a9f5e3d08cd7af195846618c03d
JosehGaks <josehwilddog@gmail.com>
2022-09-02 20:05:52 +0300

    override LoadUserByUsername method

--- src/main/java/com/example/hrBackend/service/UserDetailServiceImpl.java
+++ src/main/java/com/example/hrBackend/service/UserDetailServiceImpl.java
@@ -1,12 +1,32 @@
 package com.example.hrBackend.service;
 
+import com.example.hrBackend.domain.model.User;
+import com.example.hrBackend.domain.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.Optional;
+
 public class UserDetailServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Optional<User> user = userRepository.findByUsername(username);
+        org.springframework.security.core.userdetails.User.UserBuilder builder = null;
+
+        if (user.isPresent()){
+            User currentUser = user.get();
+            builder = org.springframework.security.core.userdetails.User.withUsername(username);
+            builder.password(currentUser.getPassword());
+            builder.authorities(currentUser.getRole());
+        }else {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return builder.build();
     }
 }
